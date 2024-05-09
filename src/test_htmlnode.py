881fcc0e4854f041ce1d22896aa1926f2ef0fbab
Nico-fox <106523048+Nico-fox@users.noreply.github.com>
2024-05-09 17:41:50 +0200

    added a test for props

--- src/test_htmlnode.py
+++ src/test_htmlnode.py
@@ -6,11 +6,15 @@ htmlNode2 = HtmlNode(tag="a", children=htmlNode1, props={"href": "http://www.kik
 htmlNode3 = HtmlNode(tag="h1", children=htmlNode2)
 
 correct_htmlNode1 = """\t~~~~~~~~~~~~~~~~~~~~~~~\ntag: p, value: Hello you!, props:None\n\t~~~~~~~~~~~~~~~~~~~~~~~\n\n"""
+correct_propsNode2 = ''' href="http://www.kikoo.fr" color="orange"'''
 
 class TestHtmlNode(unittest.TestCase):
 
     def test_no_props_no_child(self):
         return self.assertEqual(repr(htmlNode1), correct_htmlNode1)
+    
+    def test_props(self):
+        return self.assertEqual(htmlNode2.props_to_html(), correct_propsNode2)
     
 if __name__ == "__main__":
     unittest.main()
