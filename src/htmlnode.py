a209a3a0cf711a3f310719fdcc23ddd35764b7b6
Nico-fox <106523048+Nico-fox@users.noreply.github.com>
2024-05-10 00:01:37 +0200

    added ParentNode class and functions to recursively format to html parent node and children nodes

--- src/htmlnode.py
+++ src/htmlnode.py
@@ -6,7 +6,7 @@ class HtmlNode:
         self.props = props
 
     def to_html(self) -> None:
-        raise Exception(NotImplementedError)
+        raise NotImplementedError("Not yet implemented")
     
     def props_to_html(self) -> str:
         if self.props is None:
@@ -31,17 +31,47 @@ class HtmlNode:
         return children_list_string
     
 class LeafNode(HtmlNode):
-    def __init__(self, value,tag=None, props=None) -> None:
-        self.value = value
-        super().__init__(tag, self.value, None ,props)
+    def __init__(self, tag, value, props=None) -> None:
+        super().__init__(tag, value, None ,props)
 
-    def to_html(self) -> None:
+    def to_html(self) -> str:
         if not self.value:
-            raise Exception(ValueError)
+            raise ValueError("Invalid HTML: no value")
         if self.props:
             if not self.tag:
-                raise Exception(ValueError)
+                raise ValueError("Invalid HTML: no tag")
             return f"<{self.tag}{self.props_to_html()}>{self.value}</{self.tag}>"
         if self.tag:
-            return f"<{self.tag}>{self.value}<{self.tag}>"
+            return f"<{self.tag}>{self.value}</{self.tag}>"
         return f"{self.value}"
+    
+class ParentNode(HtmlNode):
+    def __init__(self, tag, children, props=None) -> None:
+        #if value:
+        #    raise ValueError("Error in HTML, parent node can't have values")
+        if not tag:
+            raise ValueError("Error in HTML, parent node need tags")
+        super().__init__(tag, None, children, props)
+    
+    def to_html_one(self) -> str:
+        if not self.tag:
+            return f"{self.value}"
+        if self.props:
+            return f"<{self.tag}{self.props_to_html()}>{self.value}</{self.tag}>"
+        return f"<{self.tag}>{self.value}</{self.tag}>"
+        
+
+    def recurse_print(self, html_string="") -> str:
+        if not self.children:
+            return f"{html_string} + {self.to_html_one()}"
+        for child in self.children:
+            if child.children:
+                html_string += child.recurse_print(html_string)
+            else:    
+                html_string += child.to_html()
+        return f"<{self.tag}>{html_string}</{self.tag}>"
+        
+
+    def to_html(self) -> str:
+        return self.recurse_print()
+    
--- src/main.py
+++ src/main.py
@@ -1,5 +1,5 @@
 from textnode import TextNode
-from htmlnode import HtmlNode, LeafNode
+from htmlnode import HtmlNode, LeafNode, ParentNode
 
 txtNode = TextNode("This is a text node", "bold", "https://www.boot.dev")
 #print(repr(txtNode))
@@ -8,6 +8,16 @@ htmlNode1 = HtmlNode(tag="p",value="Hello you!")
 htmlNode2 = HtmlNode(tag="a", children=htmlNode1, props={"href": "http://www.kikoo.fr", "color": "orange"})
 htmlNode3 = HtmlNode(tag="h1", children=htmlNode2)
 
-leafNode = LeafNode(tag="h1",value="" , props={"color": "red"})
+#leafNode = LeafNode(tag="h1",value="" , props={"color": "red"})
 
-print(leafNode.to_html())
+node = ParentNode(
+    "p",
+    [
+        LeafNode("b", "Bold text"),
+        LeafNode(None, "Normal text"),
+        LeafNode("i", "italic text"),
+        LeafNode(None, "Normal text"),
+    ],
+)
+
+print(node.to_html())
