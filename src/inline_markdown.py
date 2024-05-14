a7615fa26a1893d9dfad904c1ac352246ee79071
Nico-fox <106523048+Nico-fox@users.noreply.github.com>
2024-05-14 23:30:40 +0200

    added split_nodes_images and split_nodes_links methodes in inline_markdown and new tests

--- src/inline_markdown.py
+++ src/inline_markdown.py
@@ -4,23 +4,58 @@ from textnode import (
     text_type_bold,
     text_type_italic,
     text_type_code,
+    text_type_link,
+    text_type_image
 )
+import re
 
 def split_nodes_delimiter(old_nodes, delimiter, text_type):
-        nodeList = []
+    nodeList = []
 
-        for node in old_nodes:
-            if not isinstance(node, TextNode):
-                nodeList.append(node)
-                continue
-            if text_type == text_type_text:
-                nodeList.append(node)
-            splitted = node.text.split(delimiter)
-            if len(splitted) % 2 == 0:
-                raise Exception("no closing delimitter found")
-            for i in range(len(splitted)):
-                if i % 2 == 0:
-                    nodeList.append(TextNode(splitted[i], "text"))
-                else:
-                    nodeList.append(TextNode(splitted[i], text_type))
-        return nodeList
+    for node in old_nodes:
+        if not isinstance(node, TextNode):
+            nodeList.append(node)
+            continue
+        if text_type == text_type_text:
+            nodeList.append(node)
+        splitted = node.text.split(delimiter)
+        if len(splitted) % 2 == 0:
+            raise Exception("no closing delimitter found")
+        for i in range(len(splitted)):
+            if i % 2 == 0:
+                nodeList.append(TextNode(splitted[i], "text"))
+            else:
+                nodeList.append(TextNode(splitted[i], text_type))
+    return nodeList
+
+def extract_markdown_images(text):
+    alt_text_URL = re.findall(r"!\[(.*?)\]\((.*?)\)", text)
+    return alt_text_URL
+
+def extract_markdown_links(text):
+    anchor_text_URL = re.findall(r"\[(.*?)\]\((.*?)\)", text)
+    return anchor_text_URL
+
+def split_nodes_image(old_nodes):
+    node_text = old_nodes.text
+    markdown_images_tuples = extract_markdown_images(node_text)
+    images_nodes = []
+    for tup in markdown_images_tuples:
+        splitted = node_text.split(f"![{tup[0]}]({tup[1]})", 1)
+        if splitted:
+            images_nodes.append(TextNode(splitted[0], text_type_text))
+            images_nodes.append(TextNode(tup[0], text_type_image, tup[1]))
+            node_text = splitted[1] if len(splitted) > 1 else node_text
+    return images_nodes
+
+def split_nodes_link(old_nodes):
+    node_text = old_nodes.text
+    markdown_links_tuples = extract_markdown_links(node_text)
+    links_nodes = []
+    for tup in markdown_links_tuples:
+        splitted = node_text.split(f"![{tup[0]}]({tup[1]})", 1)
+        if splitted:
+            links_nodes.append(TextNode(splitted[0], text_type_text))
+            links_nodes.append(TextNode(tup[0], text_type_link, tup[1]))
+            node_text = splitted[1] if len(splitted) > 1 else node_text
+    return links_nodes
--- src/main.py
+++ src/main.py
@@ -13,17 +13,14 @@ htmlNode3 = HtmlNode(tag="h1", children=htmlNode2)
 
 #leafNode = LeafNode(tag="h1",value="" , props={"color": "red"})
 
-node = ParentNode(
-    "p",
-    [
-        LeafNode("b", "Bold text"),
-        LeafNode(None, "Normal text"),
-        LeafNode("i", "italic text"),
-        LeafNode(None, "Normal text"),
-    ],
-)
 
-node = TextNode("This is text with a **bold block** word", "text_type_text")
-new_nodes = split_nodes_delimiter([node], "**", "text_type_bold")
+text = "This is text with an ![image](https://storage.googleapis.com/qvault-webapp-dynamic-assets/course_assets/zjjcJKZ.png) and another ![second image](https://storage.googleapis.com/qvault-webapp-dynamic-assets/course_assets/3elNhQu.png)"
+
+node = TextNode(
+    "This is text with an ![image](https://storage.googleapis.com/qvault-webapp-dynamic-assets/course_assets/zjjcJKZ.png) and another ![second image](https://storage.googleapis.com/qvault-webapp-dynamic-assets/course_assets/3elNhQu.png)",
+    text_type_text,
+)
+new_nodes = split_nodes_image(node)
+
 
 print(new_nodes)
